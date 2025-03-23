0e8beba8c986245579b7783fde8c5edd1247e969
Sadiq Basha <78020687+sadiq-tech@users.noreply.github.com>
2025-03-23 23:55:36 +0530

    Update main.py
    edit

--- scripts/main.py
+++ scripts/main.py
@@ -3,10 +3,10 @@ import subprocess
 print("Starting IP Reputation Checker...")
 
 # Running the scripts (respective files)
-#subprocess.run(["python", "fetch_abuseipdb.py"])
-#subprocess.run(["python","enrich.py"])
+subprocess.run(["python", "fetch_abuseipdb.py"])
+subprocess.run(["python","enrich.py"])
 subprocess.run(["python", "setup_mongodb.py"])
 subprocess.run(["python", "check_virustotal.py"])
-#subprocess.run(["python", "send_email.py"])
+subprocess.run(["python", "send_email.py"])
 
 print("Process completed !")
